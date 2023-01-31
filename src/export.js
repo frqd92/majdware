bb47b9785aaab1a25e95943b7e68a7c399ffce62
Fares Qaddoura <frqd92@gmail.com>
2023-01-31 12:29:44 +0000

    checkpoint

--- src/export.js
+++ src/export.js
@@ -1,13 +1,27 @@
 import { snapshotArr, currentFactory } from "./arrayTracker";
 import elementCreator from "./utilities/createDomElement";
-import * as XLSX from 'xlsx';
+
+import * as XLSX from 'sheetjs-style';
 
 
 export default function exportFunc(){
-    const rows = visibleRows();
-    const table = exportTable(visibleRows());
+    const fromDate = visibleRows()[0].date;
+    const toDate = visibleRows()[visibleRows().length-1].date;
+    const table = exportTable(visibleRows(),[fromDate, toDate]);
+
+
 
     var workbook = XLSX.utils.table_to_book(table,{ dateNF: 'dd-mm-yyyy;@', cellDates: true, raw: true});
+
+    var sheet = workbook.Sheets[workbook.SheetNames[0]];
+    console.log(sheet)
+    sheet["B1"].s = {									
+        font: {
+          sz: 40,
+          bold: true,
+          color: { rgb: "FFFFAA00" }
+        },
+      };
     XLSX.writeFile(workbook, 'table.xlsx');
 }
 
@@ -17,32 +31,33 @@ function visibleRows(){
     const rows = document.querySelectorAll(".table-row");
         rows.forEach((elem, index)=>{
             if(!elem.classList.contains("hidden-row")){
-                arr.push(snapshotArr[index])
+                arr.push(snapshotArr[index]);
             }
         })
         return arr
 }
 
 
-function exportTable(rows){
+function exportTable(rows, dateFilter){
     const table = document.createElement("table");
-    const tableHeader = elementCreator("tr", ["class", "table-header"], false, table);
+    const infoRow = elementCreator("tr", false, false, table);
+    elementCreator("th", false, `Período: ${dateFilter[0]} > ${dateFilter[1]}`, infoRow)
+    elementCreator("th", false, currentFactory, infoRow)
 
+    const tableHeader = elementCreator("tr", false, false, table);
     for(let i=0;i<5;i++){
         switch(i){
-            case 0: elementCreator("th", false, "Data", tableHeader); break;
-            case 1: elementCreator("th", false, "Designação", tableHeader); break;
-            case 2: elementCreator("th", false, "Crédito", tableHeader); break;
-            case 3: elementCreator("th", false, "Débito", tableHeader); break;
-            case 4: elementCreator("th", false, "Saldo", tableHeader); break;
+            case 0: elementCreator("th", false, "Data",tableHeader); break;
+            case 1: elementCreator("th", false, "Designação",tableHeader); break;
+            case 2: elementCreator("th", false, "Crédito",tableHeader); break;
+            case 3: elementCreator("th", false, "Débito",tableHeader); break;
+            case 4: elementCreator("th", false, "Saldo €",tableHeader); break;
         }
     };
     for(let i=0;i<rows.length;i++){
         const tr = elementCreator("tr", false, false, table);
-
-        const date = formatDate(rows[i].date)
-     
-        elementCreator("td", false,date ,tr)
+        const date = formatDate(rows[i].date);
+        elementCreator("td", false,date,tr)
         elementCreator("td", false,`${rows[i].des}` ,tr)
         elementCreator("td", false,`${rows[i].credito}` ,tr)
         elementCreator("td", false,`${rows[i].debito}` ,tr)
@@ -53,6 +68,17 @@ function exportTable(rows){
 
 //    let tableSelect = document.getElementById("main-table");
 
+
+function informationTable(){
+
+}
+
+
+
+
+
+
+
 function formatDate(date){
     let [dd,mm,yy] = date.split("/");
     if(dd.length===1){
@@ -62,9 +88,5 @@ function formatDate(date){
         mm="0" + mm;
     }
     yy="20" + yy;
-    console.log(dd)
-    console.log(mm)
-    console.log(yy)
     return dd+"/"+mm+"/"+yy;
-
 }
